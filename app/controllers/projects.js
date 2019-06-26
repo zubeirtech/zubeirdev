63e068c696db554dbed24f93be1c4781ab5134ac
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-26 14:14:22 +0200

    add weight trakcing project

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -5,15 +5,24 @@ export default Controller.extend({
 
     cino_: false,
     dothemathright_: false,
+    myweightjourney__: false,
 
     actions: {
         cino() {
             set(this, 'dothemathright_', false);
+            set(this, 'myweightjourney__', false);
             set(this, 'cino_', true);
         },
         dothemathright() {
             set(this, 'cino_', false);
+            set(this, 'myweightjourney__', false);
             set(this, 'dothemathright_', true);
+        },
+        myweightjourney() {
+            set(this, 'cino', false);
+            set(this, 'dothemathright_', false);
+            set(this, 'myweightjourney__', true);
         }
+
     }
 });
