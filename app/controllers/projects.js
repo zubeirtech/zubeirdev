d41330bd40ca943e7fcd3efde444b1ff30bb09a0
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-04 14:12:04 +0200

    Add project

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -6,22 +6,32 @@ export default Controller.extend({
     cino_: false,
     dothemathright_: false,
     myweightjourney_: false,
+    sentrybills_:  false,
 
     actions: {
         cino() {
             set(this, 'dothemathright_', false);
             set(this, 'myweightjourney_', false);
+            set(this, 'sentrybills_', false);
             set(this, 'cino_', true);
         },
         dothemathright() {
             set(this, 'cino_', false);
             set(this, 'myweightjourney_', false);
+            set(this, 'sentrybills_', false);
             set(this, 'dothemathright_', true);
         },
         myweightjourney() {
-            set(this, 'cino', false);
+            set(this, 'cino_', false);
             set(this, 'dothemathright_', false);
             set(this, 'myweightjourney_', true);
+            set(this, 'sentrybills_', false);
+        },
+        sentrybills() {
+            set(this, 'cino_', false);
+            set(this, 'dothemathright_', false);
+            set(this, 'myweightjourney_', false);
+            set(this, 'sentrybills_', true);
         }
 
     }
