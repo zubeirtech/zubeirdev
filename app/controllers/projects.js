f662743020abfd429d460d0bd9dcefb8dabe4ac7
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-17 19:35:27 +0200

    add idyll application

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -6,7 +6,8 @@ export default Controller.extend({
     cino_: false,
     dothemathright_: false,
     myweightjourney_: false,
-    sentrybills_:  false,
+    sentrybills_: false,
+    idyll_: false,
 
     actions: {
         cino() {
@@ -14,25 +15,35 @@ export default Controller.extend({
             set(this, 'myweightjourney_', false);
             set(this, 'sentrybills_', false);
             set(this, 'cino_', true);
+            set(this, 'idyll_', false);
         },
         dothemathright() {
             set(this, 'cino_', false);
             set(this, 'myweightjourney_', false);
             set(this, 'sentrybills_', false);
             set(this, 'dothemathright_', true);
+            set(this, 'idyll_', false);
         },
         myweightjourney() {
             set(this, 'cino_', false);
             set(this, 'dothemathright_', false);
             set(this, 'myweightjourney_', true);
             set(this, 'sentrybills_', false);
+            set(this, 'idyll_', false);
         },
         sentrybills() {
             set(this, 'cino_', false);
             set(this, 'dothemathright_', false);
             set(this, 'myweightjourney_', false);
             set(this, 'sentrybills_', true);
+            set(this, 'idyll_', false);
+        },
+        idyll() {
+            set(this, 'cino_', false);
+            set(this, 'dothemathright_', false);
+            set(this, 'myweightjourney_', false);
+            set(this, 'sentrybills_', false);
+            set(this, 'idyll_', true);
         }
-
     }
 });
