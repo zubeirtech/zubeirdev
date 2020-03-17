94dd3247cb1db2f9c1f608eee2d8cbe386b149fd
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-03-17 04:46:18 +0100

    rename project

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'zubeirdev',
+    modulePrefix: 'zubeirtech',
     environment,
     rootURL: '/',
     locationType: 'auto',
